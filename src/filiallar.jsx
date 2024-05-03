fb6aeab133aa4f804ed1826931d1b2adbe3f4bdd
abbosbek23 <muratovabbosbek@gmail.com>
2024-05-03 15:53:44 +0500

    add maps

--- src/filiallar.jsx
+++ src/filiallar.jsx
@@ -7,8 +7,8 @@ const Filiallar = () => {
   return (
     <div>
         <NavbargenderOverview namepage={"Branches Overview"}/>
-        <Box sx={{height:"80vh",padding:"0 10px",width:"100%"}}>
-        <iframe src="https://www.google.com/maps/d/embed?mid=1y2DT8SagBgF_nPSxnP7pD-0YeyM52Mc&ehbc=2E312F" style={{border:"none",width:"100%"}}  height="480"></iframe>
+        <Box sx={{padding:"0 20px",width:"100%",position: "relative",overflow: "hidden",minHeight: "500px",}}>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1y2DT8SagBgF_nPSxnP7pD-0YeyM52Mc&ehbc=2E312F" style={{border:"none",width: "98%",height: "150%",position: "absolute",bottom: "-25%",left:"1%",right:"40%"}}  height="480"></iframe>
         </Box>
     </div>
   )
--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -57,7 +57,7 @@ const Sidebar = () => {
             </ul>
           </Box>
           <Box>
-            
+             
            
           </Box>
         </div>)
