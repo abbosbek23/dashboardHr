d572b89a9246c4901a96c97d97f1d7e253a92b64
abbosbek23 <muratovabbosbek@gmail.com>
2024-05-03 15:24:42 +0500

    add some edits

--- src/filiallar.jsx
+++ src/filiallar.jsx
@@ -6,9 +6,9 @@ import { Box } from '@mui/system';
 const Filiallar = () => {
   return (
     <div>
-        <NavbargenderOverview/>
-        <Box sx={{height:"80vh",marginLeft:"15px"}}>
-        <iframe src="https://www.google.com/maps/d/embed?mid=1y2DT8SagBgF_nPSxnP7pD-0YeyM52Mc&ehbc=2E312F" style={{border:"none"}} width="1070" height="480"></iframe>
+        <NavbargenderOverview namepage={"Branches Overview"}/>
+        <Box sx={{height:"80vh",padding:"0 10px",width:"100%"}}>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1y2DT8SagBgF_nPSxnP7pD-0YeyM52Mc&ehbc=2E312F" style={{border:"none",width:"100%"}}  height="480"></iframe>
         </Box>
     </div>
   )
