1cc935bf27bb51595347b9ae23cc4b9b38664acb
abbosbek23 <muratovabbosbek@gmail.com>
2024-05-08 23:25:57 +0500

    edit maps0

--- src/filiallar.jsx
+++ src/filiallar.jsx
@@ -7,8 +7,8 @@ const Filiallar = () => {
   return (
     <div>
         <NavbargenderOverview namepage={"Branches Overview"}/>
-        <Box sx={{padding:"0 20px",width:"100%",position: "relative",overflow: "hidden",minHeight: "500px",}}>
-        <iframe src="https://www.google.com/maps/d/embed?mid=1y2DT8SagBgF_nPSxnP7pD-0YeyM52Mc&ehbc=2E312F" style={{border:"none",width: "98%",height: "150%",position: "absolute",bottom: "-25%",left:"1%",right:"40%"}}  height="480"></iframe>
+        <Box sx={{height:"80vh",padding:"0 20px",width:"100%",position: "relative",overflow: "hidden",minHeight: "500px",}}>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1J0v5W1rXD3vSM08x4hE9j3d250doBrk&ehbc=2E312F" style={{border:"none",width: "98%",height: "100%",position: "absolute",left:"1%",right:"40%"}}></iframe>
         </Box>
     </div>
   )
